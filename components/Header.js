042de2044c1a94f5d947166343c0e4f871d977ef
WaliaJatin42 <waliajatin42@gmail.com>
2024-12-21 11:55:22 +0530

    added an span into header

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     <div className="container mx-auto">
       <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
         <Link href={'/'} className='font-semibold'>
-          <span className='font-bold text-2xl mr-0.5'>jatin</span> walia <span className='text-red-300 w-[10px] h-[10px] rounded-full'>.</span>
+          <span className='font-bold text-2xl mr-0.5'>Jatin</span> Walia <span className='text-red-300 w-[10px] h-[10px] rounded-full'>.</span>
         </Link>
 
         <Socials />
